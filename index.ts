f4913460bae4d34812835a1d1e63cae3c3d92686
acoolg <you@example.com>
2026-05-31 23:04:16 +0800

    a update

--- index.ts
+++ index.ts
@@ -3,42 +3,50 @@ import * as process from "node:process";
 
 const command: string | undefined = process.argv[2];
 
-console.log("start")
+console.log("start");
+
+function logger(title: string, text: any) {
+    console.log(`[${title}]: ` + text)
+}
 
 if (command == "make") {
-    fs.readdir("./holy_text", function (err: NodeJS.ErrnoException | null, files: string[]) {
-        files.forEach((e: string) => {
-        });
-    });
+    fs.readdir(
+        "./holy_text",
+        function (err: NodeJS.ErrnoException | null, files: string[]) {
+            files.forEach((e: string) => {});
+        },
+    );
 } else if (command == "test") {
     fs.readFile("test.txt", "utf8", function (err: any, data: any) {
         const code: string = data.toString();
-        console.log(code)
+        logger("code", code)
         lexer(code);
     });
 }
 
-
 // this change base string code text into an array that idk how to explain
 function lexer(code: string): void {
     var token = tokenize(code);
-    console.log("a" + token)
+    logger("token", token)
     type Token = {
         type: string;
         value: string;
     };
     function tokenize(code: string): Token[] {
+        const validFullString: string[] =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890$_".split(
+                    "",
+                );
+        return getKeyWord(code)
         function getKeyWord(text) {
-            const validFullString: string[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890$_".split("");
-        var pointer: number = 0;
-        var result: Token[] = [];
-        var key: string[] = [];
-        while (validFullString.includes(code.charAt(pointer))) {
-            key.push(code)
-            pointer += 1;
-        }
+            var pointer: number = 0;
+            var key: string[] = [];
+            while (validFullString.includes(code.charAt(pointer))) {
+                key.push(code.charAt(pointer));
+                pointer += 1;
+            }
 
-        return result;
+            return key;
         }
     }
 }
